7fe13569ccfa137efdd948c9850ce05439bb74e8
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-05-28 13:20:05 +0300

    create App Images class collaborate with flutter assets gen plugin

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesLogoOrange
+  /// assets/images/logo_orange.svg
+  static const String assetsImagesLogoOrange = "assets/images/logo_orange.svg";
+
+  /// Assets for assetsImagesLogoWhite
+  /// assets/images/logo_white.svg
+  static const String assetsImagesLogoWhite = "assets/images/logo_white.svg";
+}
